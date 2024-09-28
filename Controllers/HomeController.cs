62f6de7c9b6df1dc16e3455feaa41952a713cafe
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2024-09-28 05:32:35 +0300

    Add table UI to display data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using ContactManager.Data;
 using ContactManager.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,15 +8,18 @@ namespace ContactManager.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IQueryable<Contact> contacts = _context.Contacts.AsQueryable();
+            return View(contacts);
         }
 
         public IActionResult Privacy()
