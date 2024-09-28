d100e3bcbe4a18128f5fa3c61f99691fa67223dd
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2024-09-28 05:15:21 +0300

    Set connection with database using EF Core

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
